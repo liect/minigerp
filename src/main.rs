6b17b0812c35639577a110e0e05e0f101beb26fb
“Aicmortal” <aicmortal@gmail.com>
2023-08-31 13:43:10 +0800

    feat: erro > stderr

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use minigrep::{Config, run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     }
 }
